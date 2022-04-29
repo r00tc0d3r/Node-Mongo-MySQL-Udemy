3f7424cbf28915ca912d28a895cf1222856efbe4
r00tc0d3r <wowvaltalas123@gmail.com>
2022-04-28 22:25:51 -0300

    feat(Root): CRUD Finished
    Tracks CRUD Finished and Working

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,13 +1,43 @@
 const express = require("express");
-const { getItems, getItem, createItem } = require("../controllers/tracks");
+const {
+  getItems,
+  getItem,
+  createItem,
+  updateItem,
+  deleteItem,
+} = require("../controllers/tracks");
 const router = express.Router();
-const { validatorCreateItem } = require("../validators/tracks");
+const {
+  validatorCreateItem,
+  validatorGetItem,
+} = require("../validators/tracks");
 const customHeader = require("../middleware/customHeader");
 
 // TODO: http://localhost:PORT/tracks GET, POST, DELETE, PUT
 
+/**
+ * Listar los items
+ */
 router.get("/", getItems);
-router.post("/", validatorCreateItem, createItem);
-router.get("/:id", getItem);
 
+/**
+ * Obtener detalle de item
+ */
+router.get("/:id", validatorGetItem, getItem);
+
+/**
+ * Crear registro
+ */
+router.post("/", validatorCreateItem, createItem);
+
+/**
+ * Actualizar un registro
+ */
+router.put("/:id", validatorCreateItem, validatorGetItem, updateItem);
+module.exports = router;
+
+/**
+ * Actualizar un registro
+ */
+router.delete("/:id", validatorGetItem, deleteItem);
 module.exports = router;
--- validators/tracks.js
+++ validators/tracks.js
@@ -18,6 +18,14 @@ const validatorCreateItem = [
   },
 ];
 
+const validatorGetItem = [
+  check("id").exists().notEmpty().isMongoId(),
+  (req, res, next) => {
+    return validateResults(req, res, next);
+  },
+];
+
 module.exports = {
   validatorCreateItem,
+  validatorGetItem,
 };
